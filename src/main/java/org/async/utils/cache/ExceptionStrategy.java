9e5d71423583d7e8cb63d9b1e932fd41dc4f5a73
Barak Bar Orion <barak.bar@gmail.com>
2015-05-04 20:04:28 +0300

    Fix javadoc, update pom

--- src/main/java/org/async/utils/cache/ExceptionStrategy.java
+++ src/main/java/org/async/utils/cache/ExceptionStrategy.java
@@ -10,7 +10,7 @@ public interface ExceptionStrategy<K> {
      * 
      * @param key the key of the value that throws an exception
      * @param throwable the exception that was thrown
-     * @return true iff this <key, throwable> pair should not be cached.
+     * @return true iff this &lt;key, throwable&gt; pair should not be cached.
      */
     <T extends Throwable> boolean removeEntry(K key, T throwable);
 }
